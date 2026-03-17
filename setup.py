a6b645b7360942a5d4c065e7532e8f2c4688cd13
Zach Tong_Home_PC <zachtong@utexas.edu>
2026-03-16 19:01:29 -0500

    chore: update license classifier from MIT to proprietary

--- setup.py
+++ setup.py
@@ -233,7 +233,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research", 
-        "License :: OSI Approved :: MIT License",
+        "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
